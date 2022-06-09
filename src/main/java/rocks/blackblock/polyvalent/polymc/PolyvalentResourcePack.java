b97aabfa11f4563bdf1f328e71b0344492ffd5bb
Jelle De Loecker <jelle@elevenways.be>
2022-06-09 11:59:08 +0200

    Upgrade to PolyMC 4.1.2

--- src/main/java/rocks/blackblock/polyvalent/polymc/PolyvalentResourcePack.java
+++ src/main/java/rocks/blackblock/polyvalent/polymc/PolyvalentResourcePack.java
@@ -4,7 +4,6 @@ import io.github.theepicblock.polymc.api.resource.json.JModel;
 import io.github.theepicblock.polymc.impl.Util;
 import io.github.theepicblock.polymc.impl.resource.ResourcePackImplementation;
 import io.github.theepicblock.polymc.impl.resource.json.JModelImpl;
-import io.github.theepicblock.polymc.impl.resource.json.JModelWrapper;
 import org.apache.commons.lang3.ArrayUtils;
 
 import java.util.Objects;
@@ -17,12 +16,12 @@ public class PolyvalentResourcePack extends ResourcePackImplementation {
         if (this.getItemModel(namespace, model) == null) {
             if (Objects.equals(model, "shield")) {
                 var newModel = Util.GSON.fromJson("{\"parent\":\"builtin/entity\",\"gui_light\":\"front\",\"textures\":{\"particle\":\"block/dark_oak_planks\"},\"display\":{\"thirdperson_righthand\":{\"rotation\":[0,90,0],\"translation\":[10,6,-4],\"scale\":[1,1,1]},\"thirdperson_lefthand\":{\"rotation\":[0,90,0],\"translation\":[10,6,12],\"scale\":[1,1,1]},\"firstperson_righthand\":{\"rotation\":[0,180,5],\"translation\":[-10,2,-10],\"scale\":[1.25,1.25,1.25]},\"firstperson_lefthand\":{\"rotation\":[0,180,5],\"translation\":[10,0,-10],\"scale\":[1.25,1.25,1.25]},\"gui\":{\"rotation\":[15,-25,-5],\"translation\":[2,3,0],\"scale\":[0.65,0.65,0.65]},\"fixed\":{\"rotation\":[0,180,0],\"translation\":[-2,4,-5],\"scale\":[0.5,0.5,0.5]},\"ground\":{\"rotation\":[0,0,0],\"translation\":[4,4,2],\"scale\":[0.25,0.25,0.25]}},\"overrides\":[{\"predicate\":{\"blocking\":1},\"model\":\"item/shield_blocking\"}]}", JModelImpl.class);
-                this.setItemModel(namespace, model, new JModelWrapper(newModel));
+                this.setItemModel(namespace, model, newModel);
             } else if (Objects.equals(model, "bow")) {
                 var newModel = Util.GSON.fromJson("{\"parent\":\"builtin/entity\",\"gui_light\":\"front\",\"textures\":{\"particle\":\"block/dark_oak_planks\"},\"display\":{\"thirdperson_righthand\":{\"rotation\":[0,90,0],\"translation\":[10,6,-4],\"scale\":[1,1,1]},\"thirdperson_lefthand\":{\"rotation\":[0,90,0],\"translation\":[10,6,12],\"scale\":[1,1,1]},\"firstperson_righthand\":{\"rotation\":[0,180,5],\"translation\":[-10,2,-10],\"scale\":[1.25,1.25,1.25]},\"firstperson_lefthand\":{\"rotation\":[0,180,5],\"translation\":[10,0,-10],\"scale\":[1.25,1.25,1.25]},\"gui\":{\"rotation\":[15,-25,-5],\"translation\":[2,3,0],\"scale\":[0.65,0.65,0.65]},\"fixed\":{\"rotation\":[0,180,0],\"translation\":[-2,4,-5],\"scale\":[0.5,0.5,0.5]},\"ground\":{\"rotation\":[0,0,0],\"translation\":[4,4,2],\"scale\":[0.25,0.25,0.25]}},\"overrides\":[{\"predicate\":{\"blocking\":1},\"model\":\"item/shield_blocking\"}]}", JModelImpl.class);
-                this.setItemModel(namespace, model, new JModelWrapper(newModel));
+                this.setItemModel(namespace, model, newModel);
             } else {
-                var newModel = new JModelWrapper();
+                var newModel = new JModelImpl();
                 newModel.setParent("item/generated");
                 if (Objects.equals(model, "stick")) {
                     newModel.setParent("item/handheld");
